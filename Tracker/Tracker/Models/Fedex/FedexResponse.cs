cdf0fe11d436eaa73cfc96fd6cd719e133c5120f
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-19 11:18:21 -0500

    Fedex Response

---
+++ Tracker/Tracker/Models/Fedex/FedexResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Isatol.Tracker.Models
+{
+    public class FedexResponse
+    {
+        public TrackPackagesResponse TrackPackagesResponse { get; set; }
+    }
+
+    public class TrackPackagesResponse
+    {
+        public bool successful { get; set; }
+        public List<PackageList> PackageList { get; set; }
+    }
+
+    public class PackageList
+    {
+        public string KeyStatus { get; set; }
+        public string KeyStatusCD { get; set; }
+        public string displayShipDateTime { get; set; }
+        public string displayEstDeliveryDateTime { get; set; }
+        public List<ScanEventList> ScanEventList { get; set; }
+    }
+
+    public class ScanEventList
+    {
+        public DateTime? Date { get; set; }
+        public TimeSpan? Time { get; set; }
+        public string Status { get; set; }
+        public string StatusCD { get; set; }
+        public string ScanLocation { get; set; }
+        public bool IsDelivered { get; set; }
+    }
+}
