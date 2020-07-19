69260254875ef46b5eac3496423e162064351e6d
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-19 11:18:32 -0500

    UPS Request

---
+++ Tracker/Tracker/Models/UPS/UPS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Isatol.Tracker.Models
+{
+    public class UPS
+    {
+        public string Requester { get; set; } = "wt/trackdetails";
+        public List<string> TrackingNumber { get; set; }
+
+    }
+}
