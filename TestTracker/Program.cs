df66ba75276e680ed4bf4d9d75fbc96525947f91
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-19 11:15:24 -0500

    Using new method

--- TestTracker/Program.cs
+++ TestTracker/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace TestTracker
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            var fedex = IsatolTracker.Track.Fedex("");
-            var ups = IsatolTracker.Track.UPS("");
-            var estafeta = IsatolTracker.Track.Estafeta("");
+        {            
+            HttpClient httpClient = new HttpClient();
+            Isatol.Tracker.Track track = new Isatol.Tracker.Track(httpClient);
+            var estafeta = track.Estafeta("005588955041C705695585");
+            var ups = track.UPS("1ZA84F19DG12759281", Isatol.Tracker.Track.Locale.es_MX);
         }
     }
 }
