317061e36309291c5e6c1dcce3abffd6f7b97fc4
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-19 11:18:40 -0500

    UPS Response

---
+++ Tracker/Tracker/Models/UPS/UPSResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Isatol.Tracker.Models
+{
+    public class UPSResponse
+    {
+        public string StatusCode { get; set; }
+        public string StatusText { get; set; }
+        public List<TrackDetails> TrackDetails { get; set; }
+    }
+
+    public class TrackDetails
+    {
+        public string PackageStatus { get; set; }
+        public string PackageStatusType { get; set; }
+        public string EstimatedArrival { get; set; }
+        public string EstimatedDeparture { get; set; }
+        public List<ShipmentProgressActivities> ShipmentProgressActivities { get; set; }
+    }
+
+    public class ShipmentProgressActivities
+    {
+        public DateTime? Date { get; set; }
+        public TimeSpan? Time { get; set; }
+        public string Location { get; set; }
+        public string ActivityScan { get; set; }
+
+    }
+}
