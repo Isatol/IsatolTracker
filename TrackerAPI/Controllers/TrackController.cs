a375dcbc51f4d19ef8c21b1435425b68b4bded05
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-22 00:37:54 -0500

    Added GetTrackingModel API

--- TrackerAPI/Controllers/TrackController.cs
+++ TrackerAPI/Controllers/TrackController.cs
@@ -22,6 +22,37 @@ namespace TrackerAPI.Controllers
         }
 
         [HttpGet, Route("[action]")]
+        public async Task<IActionResult> GetTrackingModel(int companyID, string trackingNumber)
+        {
+            switch (companyID)
+            {
+                case 1:
+                    TrackingModel estafeta = await _track.EstafetaAsync(trackingNumber);
+                    return StatusCode(200, new 
+                    {
+                        trackingModel = estafeta
+                    });
+                case 2:
+                    TrackingModel fedex = await _track.FedexAsync(trackingNumber);
+                    return StatusCode(200, new 
+                    {
+                        trackingModel = fedex
+                    });
+                case 3:
+                    TrackingModel ups = await _track.UPSAsync(trackingNumber, Isatol.Tracker.Track.Locale.en_US);
+                    return StatusCode(200, new 
+                    {
+                        trackingModel = ups
+                    });
+                default:
+                    return StatusCode(200, new 
+                    {
+                        message = "No company selected"
+                    });
+            }
+        }
+
+        [HttpGet, Route("[action]")]
         public async Task<IActionResult> GetEstafeta(string trackingNumber)
         {            
             TrackingModel trackingModel = await _track.EstafetaAsync(trackingNumber);
@@ -59,6 +90,16 @@ namespace TrackerAPI.Controllers
             return StatusCode(200, new 
             {
                 message = $"Package added"
+            });
+        }
+
+        [HttpGet, Route("[action]")]
+        public async Task<IActionResult> GetUserPackages(int userID) 
+        {
+            List<TrackerDAL.Models.CompanyPackage> packages = await _tracking.GetUserPackages(userID);
+            return StatusCode(200, new 
+            {
+                packages
             });
         }
     }
