e54bb4f20cc9fb68ea9f7d61f23faf54dfdb3f84
Isaías Orozco Toledo <isatol.an@gmail.com>
2020-07-22 00:42:15 -0500

    DAL obj

---
+++ TrackerDAL/Models/CompanyPackage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerDAL.Models
+{
+    public class CompanyPackage : Company
+    {
+        public string PackageName { get; set; }
+        public int PackageID { get; set; }
+        public string TrackingNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string Event { get; set; }
+    }
+}
